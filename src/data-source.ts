1ebba21e6017e591bc9a8edb97c52c22966c787a
unknown <zaripovjasurbek2001@gmail.com>
2023-11-11 21:04:09 +0500

    remove local url

--- src/data-source.ts
+++ src/data-source.ts
@@ -17,12 +17,12 @@ import { OldOrdersEntity } from "./entities/old_order";
 
 export const AppDataSource = new DataSource({
   type: "postgres",
-  url: "postgres://postgres:123@localhost:5432/shosh_b",
-  // host: "localhost",
-  // port: 5432,
-  // username: "postgres",
-  // password: "7hEfwjl*OVRL",
-  // database: "shosh_b",
+  // url: "postgres://postgres:123@localhost:5432/shosh_b",
+  host: "localhost",
+  port: 5432,
+  username: "postgres",
+  password: "7hEfwjl*OVRL",
+  database: "shosh_b",
   synchronize: true,
   logging: false,
   entities: [
